3e219968999f7a8dbc542cf1b03c489a10176d01
VladymyrRysenko <nva@centre.kharkov.ua>
2021-10-25 00:07:44 +0300

    Create 7.3.js

---
+++ 7.3.js
@@ -0,0 +1,9 @@
+//Задание 3.
+//Написать функцию, которая создает пустой объект, но без прототипа.
+
+//const emptyObj = Object.create(null)
+
+function emptyObj() {
+  retunt Object.create(null);
+}
+emptyObj()
